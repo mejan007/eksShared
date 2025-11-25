4d54b9a46bb00fd54b0edeba13355061d5bc6fa3
Amrit Poudel <amritpoudel433@gmail.com>
2025-11-25 16:24:09 +0545

    added the role for the ec2 for eks connection

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -35,6 +35,29 @@ resource "aws_iam_role" "runner_role" {
   })
 }
 
+
+
+resource "aws_iam_role_policy" "eks_access_policy" {
+  name = "eks-access-policy"
+  role = aws_iam_role.runner_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters",
+          "eks:AccessKubernetesApi"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
 resource "aws_iam_instance_profile" "runner_profile" {
   name = "self-hosted-runner-profile"
   role = aws_iam_role.runner_role.name
