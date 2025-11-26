ba452f23f7bb1d6edb0f161f55ec650c1bc6c68c
Madhu Sharma <madhusharma9840@gmail.com>
2025-11-26 09:44:08 +0545

    Add security group rule to allow bastion access to EKS API

--- main.tf
+++ main.tf
@@ -105,3 +105,14 @@ module "ec2" {
   ]
 
 }
+
+resource "aws_security_group_rule" "bastion_to_eks_controlplane" {
+  description              = "Allow bastion to access EKS API endpoint"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+
+  security_group_id        = module.eks.cluster_security_group_id
+  source_security_group_id = module.ec2.created_security_group_id
+}
